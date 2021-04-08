17955fdd93ee9b460622045e840a28836f30f824
Muhammad Tahir A <54846767+MTahirCTOofWTRO@users.noreply.github.com>
2021-04-08 13:10:19 +0500

    Delete load_env_var.py

--- load_env_var.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-# Load MongoDB Environmental Variables
-tradbDev_str =  os.getenv("TRADBDEV_CONN_STRING")
-tradbDev_db =  os.getenv("TRADBDEV_DB")
-traDataDump_str =  os.getenv("TRADBDATADUMP_CONN_STRING")
-traDataDump_db =  os.getenv("TRADBDATADUMP_DB")
